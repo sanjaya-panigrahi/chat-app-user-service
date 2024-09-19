1f29b1a4611cbf2198f5f75558cfc26d95763d1a
Sanjaya Panigrahi <sanjayp.panigrahi@gmail.com>
2024-09-19 13:06:14 +0530

    First Commit

---
+++ src/main/java/com/chatapp/userservice/ChatappUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappUserServiceApplication.class, args);
+	}
+
+}
